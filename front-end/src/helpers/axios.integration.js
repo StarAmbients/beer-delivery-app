190203acc0c67ba89f96f105272c86fd68820b8f
Debora Schuch <starambients@gmail.com>
2022-11-18 16:51:27 +0100

    ci: :wrench: script firstconnection with db reset and node .

--- front-end/src/helpers/axios.integration.js
+++ front-end/src/helpers/axios.integration.js
@@ -7,6 +7,7 @@ const makeRequest = async (url, method, data = null, token = null) => {
     data,
     headers: { Authorization: token },
     baseURL: 'http://localhost:3001/',
+    // baseURL: 'http://beer-delivery-app-backend-production.up.railway.app',
   };
   const resultAxios = await axios(config);
   if (!resultAxios) return false;
