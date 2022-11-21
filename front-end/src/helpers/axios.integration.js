68d0551ef03cff922c4a123cc09ef65ad5fee0bd
Debora Schuch <starambients@gmail.com>
2022-11-21 22:08:02 +0100

    ci: :wrench: Configuration issue: dotenv in frontend or not

--- front-end/src/helpers/axios.integration.js
+++ front-end/src/helpers/axios.integration.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-require('dotenv').config();
-
 const makeRequest = async (url, method, data = null, token = null) => {
   const config = {
     url: `/${url}`,
@@ -9,7 +7,7 @@ const makeRequest = async (url, method, data = null, token = null) => {
     data,
     headers: { Authorization: token },
     baseURL: `${process.env.REACT_APP_BASE_URL}`,
-    // base URL: previous line and || 'http://localhost:3001/',
+    // base URL: previous line && || 'http://localhost:3001/',
   };
   const resultAxios = await axios(config);
   if (!resultAxios) return false;
