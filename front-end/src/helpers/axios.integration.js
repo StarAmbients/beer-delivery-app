6961f56abdbd8ff46e655c7387902fb5bdfb74bd
Debora Schuch <starambients@gmail.com>
2022-11-21 20:28:53 +0100

    ci: :wrench: Adding the reference -process.env- to the baseurl into the integration process

--- front-end/src/helpers/axios.integration.js
+++ front-end/src/helpers/axios.integration.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+require('dotenv').config();
+
 const makeRequest = async (url, method, data = null, token = null) => {
   const config = {
     url: `/${url}`,
     method,
     data,
     headers: { Authorization: token },
-    baseURL: `${REACT_APP_BASE_URL}`,
+    baseURL: `${process.env.REACT_APP_BASE_URL}`,
     // base URL: previous line and || 'http://localhost:3001/',
   };
   const resultAxios = await axios(config);
