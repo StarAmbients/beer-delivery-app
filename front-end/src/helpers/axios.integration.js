a4917b5cae1c33c1b14ec2b78dc84cca2762136f
Debora Schuch <starambients@gmail.com>
2022-11-21 16:05:32 +0100

    fix: :bug: Axios reac app base url template literals

--- front-end/src/helpers/axios.integration.js
+++ front-end/src/helpers/axios.integration.js
@@ -6,7 +6,7 @@ const makeRequest = async (url, method, data = null, token = null) => {
     method,
     data,
     headers: { Authorization: token },
-    baseURL: $`REACT_APP_BASE_URL` || 'http://localhost:3001/',
+    baseURL: `${REACT_APP_BASE_URL}` || 'http://localhost:3001/',
   };
   const resultAxios = await axios(config);
   if (!resultAxios) return false;
