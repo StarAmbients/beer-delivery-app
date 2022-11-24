bb33192ee913984ff972ceeeddc456c94261a7a6
Debora Schuch <starambients@gmail.com>
2022-11-24 18:27:59 +0100

    feat: :lipstick: Styling Product Card - following Figma instructions - parcial

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import PropType from 'prop-types';
 import productsStore from '../store/products.store';
+import ProductCardSComponent from '../styles/productCard.style';
 
 function ProductCard({ product }) {
   const { id, name, price, urlImage } = product;
@@ -25,40 +26,55 @@ function ProductCard({ product }) {
   }, [quantity]);
 
   return (
-    <div>
-      <p data-testid={ `customer_products__element-card-price-${id}` }>
-        {`${price.replace(/\./, ',')}`}
-      </p>
-      <img
-        data-testid={ `customer_products__img-card-bg-image-${id}` }
-        src={ urlImage }
-        alt={ name }
-      />
-      <p data-testid={ `customer_products__element-card-title-${id}` }>{name}</p>
-      <button
-        data-testid={ `customer_products__button-card-rm-item-${id}` }
-        type="button"
-        onClick={ () => (quantity === 0 ? setQuantity(0) : setQuantity(quantity - 1)) }
-      >
-        -
-      </button>
-      <input
-        data-testid={ `customer_products__input-card-quantity-${id}` }
-        type="number"
-        name="quantity"
-        value={ itemQuantity.quantity || 0 }
-        onChange={ ({ target: { value } }) => (Number(value) <= 0
-          ? setQuantity(0)
-          : setQuantity(Number(value))) }
-      />
-      <button
-        data-testid={ `customer_products__button-card-add-item-${id}` }
-        type="button"
-        onClick={ () => setQuantity(quantity + 1) }
-      >
-        +
-      </button>
-    </div>
+    <ProductCardSComponent>
+      <div className="product-card">
+        <div className="titulo">
+          <p data-testid={ `customer_products__element-card-price-${id}` }>
+            {`R$ ${price.replace(/\./, ',')}`}
+          </p>
+        </div>
+        <img
+          className="image-card"
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
+          src={ urlImage }
+          alt={ name }
+        />
+        <div
+          className="base"
+        >
+          <div
+            className="label"
+          >
+            <p data-testid={ `customer_products__element-card-title-${id}` }>{name}</p>
+          </div>
+          <button
+            data-testid={ `customer_products__button-card-rm-item-${id}` }
+            type="button"
+            onClick={
+              () => (quantity === 0 ? setQuantity(0) : setQuantity(quantity - 1))
+            }
+          >
+            -
+          </button>
+          <input
+            data-testid={ `customer_products__input-card-quantity-${id}` }
+            type="number"
+            name="quantity"
+            value={ itemQuantity.quantity || 0 }
+            onChange={ ({ target: { value } }) => (Number(value) <= 0
+              ? setQuantity(0)
+              : setQuantity(Number(value))) }
+          />
+          <button
+            data-testid={ `customer_products__button-card-add-item-${id}` }
+            type="button"
+            onClick={ () => setQuantity(quantity + 1) }
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </ProductCardSComponent>
   );
 }
 
---
+++ front-end/src/styles/productCard.style.js
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+
+const ProductCardSComponent = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap");
+  
+  /* display: flex;
+  flex-direction: column;
+  background: #E5E5E5;
+  Product List */
+
+  position: absolute;
+  width: 1800px;
+  height: 1166px;
+  left: 60px;
+  top: 159px;
+  
+  
+  .product-card {
+    /* font-family: "Roboto", sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+    width: 175px;
+    height: 240px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    margin-top: 30px;
+    overflow: hidden;
+    border-radius: 4px;
+
+    background: #EAF1EF;
+    border: 1px solid #B1C2BE;
+
+    Card Produto */
+
+    position: absolute;
+    width: 378px;
+    height: 486px;
+    left: 80px;
+    top: 0px;
+
+    .titulo {
+      /* Título */
+      position: absolute;
+      left: 0%;
+      right: 0%;
+      top: 0%;
+      bottom: 0%;
+
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 36px;
+      line-height: 42px;
+      display: flex;
+      align-items: left;
+      text-indent: 15px;
+
+      /* Texto Dark */
+      color: #001813;
+    }
+    
+    .image-card {
+      /* Image */
+      position: absolute;
+      height: 357px;
+      left: 0.28%;
+      right: 0.28%;
+      top: 2px;
+
+      /* height: 70%;
+      max-width: 100%;
+      overflow: hidden;
+      object-fit: fit-content; */
+
+      .label {
+        /* Label */
+        position: absolute;
+        left: 0%;
+        right: 0%;
+        top: 0%;
+        bottom: 0%;
+
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 24px;
+        line-height: 28px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+
+        /* Texto Dark */
+        color: #001813;
+      }
+    }
+    .base {
+      /* Base */
+      box-sizing: border-box;
+
+      position: absolute;
+      left: 0%;
+      right: 0%;
+      top: 0%;
+      bottom: 0%;
+
+      background: #EAF1EF;
+      border: 1px solid #B1C2BE;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+  }
+`;
+
+export default ProductCardSComponent;
