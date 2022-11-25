143724f09e22ade318dba72e44683516bf1d2d24
Debora Schuch <starambients@gmail.com>
2022-11-25 17:56:39 +0100

    style: :lipstick: CSS Variables with Styled Components
    https://codesandbox.io/s/css-variables-with-styled-components-1q887?file=/src/components/index.js

--- front-end/src/styles/productCard.style.js
+++ front-end/src/styles/productCard.style.js
@@ -3,11 +3,23 @@ import styled from 'styled-components';
 const ProductCardSComponent = styled.div`
   @import url("https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap");
   
+  /* Colors */
+  --colors-light-text: #F2FFFC;
+  --colors-dark-text: #001813;
+  --colors-primary: #036B52;
+  --colors-secondary: #2FC18C;
+  --colors-tertiary: #421981;
+  --colors-quaternary: #056CF9;
+  --colors-quinary: #7B61FF;
+  --colors-light-grey: #E5E5E5;
+  --colors-dark-grey: #828282;
+
   /* display: flex;
   flex-direction: column;
   background: #E5E5E5;
   Product List */
-
+  display: flex;
+  flex-direction: column;
   position: absolute;
   width: 1800px;
   height: 1166px;
