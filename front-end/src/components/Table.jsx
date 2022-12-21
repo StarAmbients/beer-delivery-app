e13749c82e19e7658faa3b29ae045ad8fd9d5f96
Debora Schuch <starambients@gmail.com>
2022-12-21 16:18:09 +0100

    style: :lipstick: Add horizontal and vertical scrolling feature to table

--- front-end/src/components/Table.jsx
+++ front-end/src/components/Table.jsx
@@ -1,95 +1,114 @@
 import React from 'react';
 import PropType from 'prop-types';
+import { Trash } from 'phosphor-react';
 import productsStore from '../store/products.store';
-import TableSComponent from '../styles/table.style';
+import { TableSComponent, TrashSComponent } from '../styles/table.style';
 
 function Table({ page }) {
   const { cart, removeFromCart } = productsStore((state) => state);
   return (
     <TableSComponent>
       <table>
-        <thead>
+
+        <table className="title-table">
           <tr>
-            <th>Item</th>
-            <th>Descrição</th>
-            <th>Quantidade</th>
-            <th>Valor Unitário</th>
-            <th>Sub-total</th>
+            <th className="item">Item</th>
+            <th className="description">Descrição</th>
+            <th className="quantity">Quantidade</th>
+            <th className="unit-value">Valor Unitário</th>
+            <th className="sub-total">Sub-total</th>
             {page === 'checkout' && <th>Remover Item</th>}
           </tr>
-        </thead>
-        <tbody>
-          {cart?.map((c, index) => (
-            <tr key={ c.name }>
-              <td
-                data-testid={
-                  page === 'checkout'
-                    ? `customer_checkout__element-order-table-item-number-${index}`
-                    : `customer_order_details__element-order-table-item-number-${index}`
-                }
+        </table>
+        <div
+          className="scroll"
+        >
+          <table>
+            {cart?.map((c, index) => (
+              <tr
+                className="product-line"
+                key={ c.name }
               >
-                { index + 1 }
-              </td>
-              <td
-                className="product-name"
-                data-testid={
-                  page === 'checkout'
-                    ? `customer_checkout__element-order-table-name-${index}`
-                    : `customer_order_details__element-order-table-name-${index}`
-                }
-              >
-                { c.name }
-              </td>
-              <td
-                data-testid={
-                  page === 'checkout'
-                    ? `customer_checkout__element-order-table-quantity-${index}`
-                    : `customer_order_details__element-order-table-quantity-${index}`
-                }
-              >
-                { page === 'checkout' ? c.quantity : c.SalesProducts.quantity }
-              </td>
-              <td
-                data-testid={
-                  page === 'checkout'
-                    ? `customer_checkout__element-order-table-unit-price-${index}`
-                    : `customer_order_details__element-order-table-unit-price-${index}`
-                }
-              >
-                { c.price.replace(/\./g, ',') }
-              </td>
-              <td
-                data-testid={
-                  page === 'checkout'
-                    ? `customer_checkout__element-order-table-sub-total-${index}`
-                    : `customer_order_details__element-order-table-sub-total-${index}`
-                }
-              >
-                { page === 'checkout'
-                  ? Number(c.price * c.quantity).toFixed(2)
-                    .replace(/\./g, ',')
-                  : Number(c.price * c.SalesProducts.quantity).toFixed(2)
-                    .replace(/\./g, ',')}
-              </td>
-              {
-                page === 'checkout' && (
-                  <td>
-                    <button
-                      type="button"
-                      value={ c.name }
-                      data-testid={
-                        `customer_checkout__element-order-table-remove-${index}`
-                      }
-                      onClick={ () => removeFromCart(c.id) }
+                <td
+                  className="product-item"
+                  data-testid={
+                    page === 'checkout'
+                      ? `customer_checkout__element-order-table-item-number-${index}`
+                      : `customer_order_details__element-order-table-item-number-${index}`
+                  }
+                >
+                  { index + 1 }
+                </td>
+                <td
+                  className="product-description"
+                  data-testid={
+                    page === 'checkout'
+                      ? `customer_checkout__element-order-table-name-${index}`
+                      : `customer_order_details__element-order-table-name-${index}`
+                  }
+                >
+                  { c.name }
+                </td>
+                <td
+                  className="product-quantity"
+                  data-testid={
+                    page === 'checkout'
+                      ? `customer_checkout__element-order-table-quantity-${index}`
+                      : `customer_order_details__element-order-table-quantity-${index}`
+                  }
+                >
+                  { page === 'checkout' ? c.quantity : c.SalesProducts.quantity }
+                </td>
+                <td
+                  className="product-value"
+                  data-testid={
+                    page === 'checkout'
+                      ? `customer_checkout__element-order-table-unit-price-${index}`
+                      : `customer_order_details__element-order-table-unit-price-${index}`
+                  }
+                >
+                  { c.price.replace(/\./g, ',') }
+                </td>
+                <td
+                  className="product-subtotal"
+                  data-testid={
+                    page === 'checkout'
+                      ? `customer_checkout__element-order-table-sub-total-${index}`
+                      : `customer_order_details__element-order-table-sub-total-${index}`
+                  }
+                >
+                  { page === 'checkout'
+                    ? Number(c.price * c.quantity).toFixed(2)
+                      .replace(/\./g, ',')
+                    : Number(c.price * c.SalesProducts.quantity).toFixed(2)
+                      .replace(/\./g, ',')}
+                </td>
+                {
+                  page === 'checkout' && (
+                    <td
+                      className="product-remove"
                     >
-                      Remover
-                    </button>
-                  </td>
-                )
-              }
-            </tr>
-          ))}
-        </tbody>
+                      <button
+                        className="button-remove"
+                        type="button"
+                        value={ c.name }
+                        data-testid={
+                          `customer_checkout__element-order-table-remove-${index}`
+                        }
+                        onClick={ () => removeFromCart(c.id) }
+                      >
+                        Remover
+                        <TrashSComponent>
+                          <Trash />
+                        </TrashSComponent>
+                      </button>
+                    </td>
+                  )
+                }
+              </tr>
+            ))}
+          </table>
+        </div>
       </table>
     </TableSComponent>
   );
--- front-end/src/styles/table.style.js
+++ front-end/src/styles/table.style.js
@@ -4,6 +4,8 @@ const TableSComponent = styled.section`
   border-radius: 4px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   width: 100%;
+  max-height: 1200px;
+  /* overflow-y: scroll; */
 
   table {
     width: 100%;
@@ -11,16 +13,110 @@ const TableSComponent = styled.section`
     justify-content: center;
     flex-direction: row;
   }
+
+  th {
+    /* position: sticky; */
+    top: 0;
+  }
+
   td {
     background-color: violet;
     text-align: center;
+    height: 55px;
   }
 
-  .product_name {
+  .product-item {
+    background-color: #036B52;
+    color: white;
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+
+  .product-description {
     /* important */
     text-align: left;
+    padding-left: 30px;
+    max-width: 170px;
+    background-color: #EAF1EF;
+  }
+
+  .product-quantity {
+    background-color: #036B52;
     color: white;
   }
-  `;
 
-export default TableSComponent;
+  .product-value {
+    background-color: #421981;
+    color: white;
+  }
+
+  .product-subtotal {
+    color: #F2FFFC;
+    background-color: #056CF9;
+
+  }
+  
+  .product-line{
+      /* background-color: #2FC18C; */
+      border-radius: 15px;
+    }
+
+  .product-remove {
+    background-color: #2FC18C;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0px;
+    /* width: 65px; */
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+  }
+
+  .button-remove {
+    display: flex;
+    background-color: red;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    color: white;
+    width: 7vw;
+    border: none;
+  }
+  .button-remove:hover {
+    cursor: pointer;
+  }
+
+  .scroll {
+    max-height: 200px;
+    overflow: auto;
+  }
+
+  .title-table tr {
+    display: flex;
+    justify-content: flex-start;
+    font-size: 18px;
+  }
+  .item {
+    margin-left: 10px;
+  }
+  .description {
+    margin-left: 350px;
+  }
+  .quantity {
+    margin-left: 290px;
+  }
+  .unit-value {
+    margin-left: 40px;
+  }
+  .sub-total {
+    margin-left: 115px;
+    margin-right: 148px;
+  }
+
+`;
+
+const TrashSComponent = styled.section`
+      color: red;
+`;
+
+export { TableSComponent, TrashSComponent };
