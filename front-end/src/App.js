863dd4802f0b6680129daa349075f389dff5ccd9
Debora Schuch <starambients@gmail.com>
2023-02-02 18:23:33 +0100

    feat: :construction: Integrate Swagger to this react project
    swagger-react-ui

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import GlobalStyle from './GlobalStyle';
+// import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+// import GlobalStyle from './GlobalStyle';
 import IndexRouter from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyle />
+      {/* <GlobalStyle /> */}
       <IndexRouter />
+      {/* <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />
+      <SwaggerUI url="http://localhost:3000" /> */}
     </BrowserRouter>
   );
 }
