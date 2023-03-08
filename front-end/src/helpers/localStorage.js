ed0abea3453d9dbcdcf601bb70107fd2825ade3c
Debora Schuch <starambients@gmail.com>
2023-03-08 12:43:09 +0100

    fix: :bug: Allow login only  if user object is not empty

--- front-end/src/helpers/localStorage.js
+++ front-end/src/helpers/localStorage.js
@@ -2,8 +2,16 @@ export const setUserLocalStorage = (data) => {
   localStorage.setItem('user', JSON.stringify({ ...data }));
 };
 
+// export const getUserLocalStorage = () => {
+//   const localStorageGET = JSON.parse(localStorage.getItem('user'));
+//   return localStorageGET;
+// };
+
 export const getUserLocalStorage = () => {
   const localStorageGET = JSON.parse(localStorage.getItem('user'));
+  if (localStorageGET == null) { // verifica se localStorageGET é nulo ou indefinido
+    return {}; // retorna um objeto vazio como valor padrão
+  }
   return localStorageGET;
 };
 
